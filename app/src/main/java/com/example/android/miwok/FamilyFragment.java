288840a32fff97acc25c128c40c8d5783f5c69e4
Nam <namlu1205@gmail.com>
2016-10-15 12:58:52 +1100

    5 Add FamilyFragment.java, refactor FamilyActivity.java to host fragment

---
+++ app/src/main/java/com/example/android/miwok/FamilyFragment.java
@@ -0,0 +1,171 @@
+package com.example.android.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FamilyFragment extends Fragment {
+
+    /*
+     * @param mediaPlayer Create a MediaPlayer object
+     * @param audioManager Handles audio focus when playing a sound file
+     */
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+
+    public FamilyFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list_layout, container, false);
+
+        /* Insert all the code from the NumberActivity’s onCreate() method after the setContentView method call */
+
+        // Setup the AudioManger to request audio focus
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        // An ArrayList of words
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        // Add Strings element to words ArrayList
+        words.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("mother","әṭa", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("son","angsi", R.drawable.family_son, R.raw.family_son));
+        words.add(new Word("daughter","tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("older brother","taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
+        words.add(new Word("younger brother","chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        words.add(new Word("older sister","teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new Word("younger sister","kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        words.add(new Word("grandmother","ama", R.drawable.family_grandmother, R.raw.family_grandmother));
+        words.add(new Word("grandfather","paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
+
+        // An ArrayAdapter whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single TextView, which the adapter will set to
+        // display a single word.
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_family);
+
+        // Find the ListView object in the view hierarchy of the Activity.
+        // There should be a ListView with the view ID called list, which is declared in the
+        // word_list_layout.xml layout file.
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        // Make the ListView use the ArrayAdapter we created above, so that the
+        // ListView will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the ListView object and pass in
+        // 1 argument, which is the ArrayAdapter with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
+
+        // Setup ListView to use OnItemClickListener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Release any MediaPlayer resources before initializing new audio
+                releaseMediaPlayer();
+
+                // Get the Word object at the given position the user clicked on
+                Word word = words.get(position);
+
+                /* Request audio focus for playback
+                 * requestAudioFocus(AudioManager.OnAudioFocusChangeListener l, int streamType, int durationHint)
+                 */
+                int result = audioManager.requestAudioFocus(onAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request temporary  focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Start playback.
+                    // Create and setup the MediaPlayer for the audio resource associated
+                    // with the current word
+                    mediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResId());
+                    mediaPlayer.start();
+
+                    // Set listener on MediaPlayer so that we can close it when sound has finished playing
+                    mediaPlayer.setOnCompletionListener(completionListener);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    // Create OnCompletionListener object that can be used throughout
+    // This listener is triggered when MediaPlayer has completed playing a song
+    // If triggered, will release the MediaPlayer resource
+    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    /*
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // Pause playback
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // Resume playback
+                mediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // Stop playback
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    /*
+     * Upon onStop(), release the MediaPlayer resources
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+
+            // Abandon audio focus when playback complete
+            audioManager.abandonAudioFocus(onAudioFocusChangeListener);
+        }
+    }
+}
